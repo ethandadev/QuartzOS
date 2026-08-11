63de6201e9fc30a7da1b75c9022ec2b4d1a3b2b0
Ethan7080 <ethandadev@gmail.com>
2026-08-11 21:48:39 +0800

    start working on capture helper in swift because pyobjc is kinda broken

--- native/capture_helper.swift
+++ native/capture_helper.swift
@@ -1,2 +1,19 @@
 import ScreenCaptureKit
 
+let content = try await SCShareableContent.current
+let targetAppName = "PyCharm"
+var targetAppWindow: SCWindow? = nil
+
+
+for win in content.windows {
+    if let app = win.owningApplication{
+        if win.isOnScreen && win.frame.width > 10 && win.frame.height > 10 && app.applicationName == targetAppName{
+            targetAppWindow = win
+            print("[Capture Helper] success :D")
+            break
+        }
+    } else {
+    print("[Capture Helper] Could not find " + targetAppName)
+
+    }
+}
